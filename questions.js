3b7da4f664682ef517ade0cba5f1601b3325f624
Deeprathod1611 <153952708+Deeprathod1611@users.noreply.github.com>
2024-02-15 22:55:54 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+       {
+       numb: 1,
+       question: "What of the following is the default value of a local variable?",
+       answer: "Not assigned",
+       options: [
+               "null",
+               "0",
+               "Depends upon the type of variable",
+              "Not assigned"
+       ]
+     },
+     {
+       numb: 2,
+       question: "What is the size of short variable?",
+       answer: "16 bit",
+       options: [
+              "8 bit",
+              "16 bit",  
+              "32 bit",
+              "64 bit"
+       ]
+     },
+     {
+      numb: 3,
+      question: " what is polymorphism?",
+      answer:"Polymorphism is the ability of an object to take on many forms",
+      options:["create a objects",
+      "calls data members",
+      "inherite class",
+      "Polymorphism is the ability of an object to take on many forms"
+
+      ]
+     },
+     {
+       numb: 4,
+       question: "Which of the following function of Array object represents the source code of an object?",
+       answer: "toSource()",
+       options: [
+         "toSource()",
+         "splice()",
+         "toString()",
+         "unshift()"
+       ]
+     },
+     {
+       numb: 5,
+       question: "Which built-in method returns the calling string value converted to lower case?",
+       answer: "toLowerCase()",
+       options: [
+         "toLowerCase()",
+         "toLower()",
+         "changeCase(case)",
+         "None of the above."
+       ]
+     },
+      {
+        numb: 6,
+       question: "What does HTML stand for?",
+       answer: "Hyper Text Markup Language",
+       options: [
+         "Hyper Text Preprocessor",
+         "Hyper Text Markup Language",
+         "Hyper Text Multiple Language",
+         "Hyper Tool Multi Language"
+       ]
+     },
+     {
+        numb: 7,
+       question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+       options: [
+         "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+       ]
+     },
+     {
+       numb: 8,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+         "Hyper Text Preprocessor",
+         "Hyper Text Markup Language",
+         "Hyper Text Multiple Language",
+         "Hyper Tool Multi Language"
+       ]
+      },
+      {
+       numb: 9,
+       question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+   options: [
+         "Hyper Text Preprocessor",
+         "Hyper Text Markup Language",
+         "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+       ]
+     },
+     {
+        numb: 10,
+        question: "What does HTML stand for?",
+       answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+         "Hyper Text Markup Language",
+         "Hyper Text Multiple Language",
+         "Hyper Tool Multi Language"
+       ]
+      },
+   
+   ];
